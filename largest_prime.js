92e5e756be95a8dc718bedbf53d4e3da37d62066
KingHippo <listentoween@comcast.net>
2015-02-10 18:14:58 -0500

    Finished Euler problem 3 and updated index.html

--- largest_prime.js
+++ largest_prime.js
@@ -1,8 +1,24 @@
-var max = 600851475143;
-var primeFactor = 0;
-for (i = 1; i < target; i++) {
-    if (target % i === 0) {
-        primeFactor = i;
+function findPrime() {
+
+	var sumForm = document.forms["primeForm"];
+
+	var num = sumForm.elements["primenum"].value;
+
+	var newNum = num;
+
+    var largestPrime = 0;
+
+    var counter = 2;
+    while (counter * counter <= newNum) {
+        if (newNum % counter == 0) {
+            newNum = newNum / counter;
+        } else {
+            counter++;
+        }
     }
+    if (newNum > largestPrime) {
+        largestPrime = newNum;
+    }
+    document.getElementById('prime').innerHTML = "The largest prime number of "
+    												+ num + " is " + largestPrime;
 }
-console.log(primeFactor);
